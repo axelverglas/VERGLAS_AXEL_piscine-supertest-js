46ac681c2bd2712f71c58b09482829f9abacfffe
axel <axel@rockusupport.com>
2023-11-02 16:31:12 +0100

    fix

--- s2/exercice1/index.js
+++ s2/exercice1/index.js
@@ -1,6 +1,6 @@
 module.exports = function dateIlYADixAns() {
   let dateActuelle = new Date();
-  let datePassée = new Date(
+  let datePassee = new Date(
     dateActuelle.setFullYear(dateActuelle.getFullYear() - 10)
   );
   return datePassée.toISOString().split("T")[0]; // Retourne la date au format 'YYYY-MM-DD'
